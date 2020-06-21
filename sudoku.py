99034224ca920730512a14dce7fbd3ca0273cc3d
Yuriko <ytamura@gmail.com>
2020-06-21 12:39:52 -0700

    chore: python 3 compatibility, cleanup

--- sudoku.py
+++ sudoku.py
@@ -69,8 +69,8 @@ def check_puzzle_valid(puzzle):
 
 
 def _get_used_in_square(puzzle, i, j):
-    i /= 3
-    j /= 3
+    i = int(i/3)
+    j = int(j/3)
     small_L = 3
     used = []
     for k in range(small_L):
@@ -80,15 +80,6 @@ def _get_used_in_square(puzzle, i, j):
             if x > 0:
                 used.append(x)
     return used
-
-
-def _get_all_possible_numbers(puzzle):
-    possibles = np.empty([L, L], dtype=object)
-
-    for i in range(L):
-        for j in range(L):
-            possibles[i, j] = get_possible_numbers(puzzle, i, j)
-    return possibles
 
 
 def get_possible_numbers(puzzle, i, j):
@@ -104,35 +95,16 @@ def get_possible_numbers(puzzle, i, j):
                 set(used_in_square))
 
 
-def solve_puzzle(puzzle, possibles, n=0):
-    i = n / L
+def solve_puzzle(puzzle, n=0):
+    '''Solve puzzle recursively.
+
+    :param puzzle: puzzle, a 2D numpy array
+    :param n: flattened index in puzzle
+
+    :return: the solved puzzle or False
+    '''
+    i = int(n / L)
     j = n % L
-    # sys.stdout.write('.')
-    # sys.stdout.flush()
-
-    if check_puzzle_valid(puzzle):
-        return puzzle
-    elif n >= L*L:
-        return False
-    if len(possibles[i, j]) == 0:
-        return False
-    else:
-        # print('%s (%d,%d) %s' % (puzzle, i, j, possibles[i, j]))
-        orig_possibles = possibles[i, j]
-        for possible in orig_possibles:
-            puzzle[i, j] = possible
-            updated_possibles = _get_all_possible_numbers(puzzle)
-            sol = solve_puzzle(puzzle.copy(), updated_possibles, n+1)
-            if type(sol) != bool:
-                return sol
-    return False
-
-
-def solve_puzzle_faster(puzzle, n=0):
-    i = n / L
-    j = n % L
-    # sys.stdout.write('.')
-    # sys.stdout.flush()
 
     if check_puzzle_valid(puzzle):
         return puzzle
@@ -143,10 +115,9 @@ def solve_puzzle_faster(puzzle, n=0):
         return False
     else:
         # print('%s (%d,%d) %s' % (puzzle, i, j, possibles))
-        orig_possibles = possibles
-        for possible in orig_possibles:
+        for possible in possibles:
             puzzle[i, j] = possible
-            sol = solve_puzzle_faster(puzzle.copy(), n+1)
+            sol = solve_puzzle(puzzle.copy(), n+1)
             if type(sol) != bool:
                 return sol
     return False
@@ -162,15 +133,9 @@ def print_puzzle(puzzle, title=''):
 if __name__ == '__main__':
     print_puzzle(puzzle, title='Puzzle')
 
-    # print(check_puzzle_valid(puzzle))
-
-    possibles = _get_all_possible_numbers(puzzle)
-    # print_puzzle(possibles)
-    # print possibles[0, 0]
     print('Solving...')
     start = time.time()
-    # solution = solve_puzzle(puzzle, possibles, 0)
-    solution = solve_puzzle_faster(puzzle, 0)
+    solution = solve_puzzle(puzzle, 0)
     elapsed_time = time.time() - start
     print_puzzle(solution, title='Solution')
     print('Elapsed time: %ss' % elapsed_time)
